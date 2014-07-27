ac2f0e5c01eae5cf4a6ba1befed40b39b6e02c5a
SimpleMonk <simplemonk08@gmail.com>
2014-07-27 18:59:34 +0200

    Gulp Fix path in config and clean templates

--- tasks/templates.js
+++ tasks/templates.js
@@ -2,21 +2,28 @@ var config = require('./config.js'),
     gulp = require('gulp'),
     gutil = require('gulp-util'),
     clean = require('./util.js').clean,
+    rimrafMain = require('rimraf'),
+    newer = require('gulp-newer'),
     connect = require('gulp-connect'),
     runSequence = require('run-sequence'),
     src = config.path.src,
     dev = config.path.development;
 
+
+
+gulp.task('prepare-templates', function (cb) {
+    runSequence('clean-templates', 'copy-templates', cb);
+});
+
 gulp.task("copy-templates", function () {
-    runSequence('clean-templates', function () {
-        gulp.src(src.templates)
-            .pipe(gulp.dest(dev.templates))
-            .pipe(connect.reload())
-            .on('end', function () {
-                gutil.log('successfully copied html files')
-            })
-            .on('error', onError);
-    });
+    gulp.src(src.templates)
+        .pipe(newer(dev.templates))
+        .pipe(gulp.dest(dev.templates))
+        .pipe(connect.reload())
+        .on('end', function () {
+            gutil.log('successfully copied html files')
+        })
+        .on('error', onError);
 });
 
 gulp.task("copy-index-file", ['remove-index-file'], function () {
@@ -29,8 +36,8 @@ gulp.task("copy-index-file", ['remove-index-file'], function () {
         .on('error', onError);
 });
 
-gulp.task('clean-templates', function () {
-    clean(dev.templates + "**/*");
+gulp.task('clean-templates', function (cb) {
+    rimrafMain('./development/templates', cb);
 });
 
 gulp.task('remove-index-file', function () {
