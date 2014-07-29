43c7633cf1bc88058f964fc9b4387f9ce9521f09
SimpleMonk <simplemonk08@gmail.com>
2014-07-29 10:48:01 +0200

    Basic isolate scope function reference

--- src/js/calculator/directives/calculator-directive.js
+++ src/js/calculator/directives/calculator-directive.js
@@ -1,3 +1,22 @@
-/**
- * Created by lpsentilkumaran on 29/7/14.
- */
+"use strict";
+
+var angular = require("angular");
+
+var calculatorDirective = function () {
+    return{
+        restrict: 'A',
+        scope: {
+            number1: '=',
+            number3: '=',
+            calculate: '&'
+        },
+        template: '<input type="text" ng-model="number1"/>{{number1}}' +
+            '<input type="text" ng-model="number3"/>{{number3}}',
+        link: function (scope, element, attributes, controller) {
+
+        }
+    };
+};
+
+
+module.exports = calculatorDirective;
